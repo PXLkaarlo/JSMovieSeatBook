ddd9a59bcf1b896ad5537de42c9e518bd7dad2e5
Victor Immo <victor.l.immo@gmail.com>
2026-01-27 15:07:40 +0100

    Still working on JSON import and usage

--- script/script.js
+++ script/script.js
@@ -5,16 +5,27 @@ const count = document.getElementById('count'); // use innerHTML to show seats s
 const total = document.getElementById('total'); // use innerHTML to show payment total
 const movieSelect = document.getElementById('movie');
 
-// const movieDB = JSON.parse(localStorage.getItem('selectedMovie'));
 
-// import * as dataBase from './db.json' assert { type: 'json' };
+fetch('movieDB.json')
+    .then(response => response.json())
+    .then(movieDB => {
+        let movieData = movieDB.movies;
+    })
+    .catch(error => console.error('Error fetching movie data:', error));
+    console.log(movieData);
 
-// console.log(dataBase);
 
 
-for (let option of movieSelect.options) {
-    // if (movieDB !== null) {
-}
+// for (let o of movieDB.movies) {
+//     const option = document.createElement('option');
+
+//     option.value = o.price;
+//     option.text = `${o.title} (${o.price} kr)`;
+
+//     movieSelect.appendChild(option);
+
+//     console.log(option);
+// }
 
 
 let ticketPrice = +movieSelect.value; // + converts string to number

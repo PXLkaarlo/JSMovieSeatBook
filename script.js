d0330f8c79d7cf091b6d934c9975c26cb95f64d6
Victor Immo <victor.l.immo@gmail.com>
2026-02-13 14:20:15 +0100

    Keep script.js and style.css in the root or pipeline will not work

---
+++ script.js
@@ -0,0 +1,26 @@
+const screen = document.querySelector('.screen');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count'); // use innerHTML to show seats selected
+const total = document.getElementById('total'); // use innerHTML to show payment total
+const movieSelect = document.getElementById('movie');
+
+import { loadMovies } from './src/script/data/repository.js';
+import { fillMovieList, updateSeatAndPrice, updatePoster } from './src/script/data/engine.js';
+
+const movieList = await loadMovies();
+let ticketPrice = +movieSelect.value; // + converts string to number
+
+fillMovieList(movieList, movieSelect, screen);
+
+
+for (let seat of seats) {
+  seat.addEventListener('click', () => {
+    seat.classList.toggle('selected');
+    updateSeatAndPrice(ticketPrice ,movieSelect, count, total);
+  });
+}
+
+movieSelect.addEventListener('change', () => {
+  updateSeatAndPrice(ticketPrice ,movieSelect, count, total),
+  updatePoster(screen, movieList[movieSelect.selectedIndex])
+});

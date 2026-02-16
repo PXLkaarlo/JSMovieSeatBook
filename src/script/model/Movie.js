e4986853792902568a2ebd9e284f0bee613e6220
PXLkaarlo <victor.l.immo@gmail.com>
2026-02-16 11:33:16 +0100

    temporary commit message

--- src/script/model/Movie.js
+++
@@ -1,8 +0,0 @@
-export class Movie {
-    constructor(title, year, price, poster) {
-        this.title = title;
-        this.year = year;
-        this.price = price;
-        this.poster = poster;
-    }
-}

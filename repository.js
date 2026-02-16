353211de5c548fe7a9e426d3655c8b24ed8ff5e4
PXLkaarlo <victor.l.immo@gmail.com>
2026-02-16 11:52:22 +0100

    Plese work. T-T

--- repository.js
+++ repository.js
@@ -1,14 +1,16 @@
 import { Movie } from './movie.js';
 
 export async function loadMovies() {
-    const response = await fetch('/src/database/movies.json');
-    const data = await response.json();
+  const response = await fetch('./movies.json');
+  const data = await response.json();
 
-    const sortArray = data.map(
-        m => new Movie(m.title, m.year, m.price, m.poster)
-    );
+  const sortArray = data.map(
+    (m) => new Movie(m.title, m.year, m.price, m.poster)
+  );
 
-    return sortArray.sort(function(a, b){return b.year - a.year});
+  return sortArray.sort(function (a, b) {
+    return b.year - a.year;
+  });
 }
 
 // Horrible naming scheme, I know
